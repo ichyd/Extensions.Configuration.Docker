00f0a6310ad8090590b54d60c6525deb31e1060e
Isaac Hoey <48498580+hoeyi@users.noreply.github.com>
2026-07-12 15:36:06 -0400

    feat: version 1.0

---
+++ src/lib/Extensions.Configuration.Docker/ConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Hi_saac.Extensions.Configuration.Docker
+{
+    [ExcludeFromCodeCoverage]
+    public static class ConfigurationManagerExtensions
+    {
+        /// <summary>
+        /// Add a <see cref="DockerSecretsProvider"/> to this configuration.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="secretsPath">Path to the secrets. Default is <em>/run/secrets</em>.</param>
+        /// <returns>This <see cref="IConfigurationBuilder"/> instance for method chaining.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static IConfigurationBuilder AddDockerSecrets(
+            this IConfigurationBuilder builder, string? secretsPath = "/run/secrets")
+        {
+            ArgumentException.ThrowIfNullOrEmpty(secretsPath);
+            
+            builder.Add(new DockerSecretsSource(secretsPath));
+
+            return builder;
+        }
+    }
+}
---
+++ src/lib/Extensions.Configuration.Docker/DockerSecretsSource.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+
+namespace Hi_saac.Extensions.Configuration.Docker
+{
+    /// <summary>
+    /// Represents a <b>docker secrets</b> source of configuration key/values for an application.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class DockerSecretsSource : IConfigurationSource
+    {
+        private string _secretsPath;
+
+        /// <summary>
+        /// Constructs a new instance of <see cref="DockerSecretsSource"/>.
+        /// </summary>
+        /// <param name="secretsPath">Path to the secrets location.</param>
+        public DockerSecretsSource(string secretsPath)
+        {
+            _secretsPath = secretsPath;
+        }
+
+        /// <inheritdoc/>
+        public IConfigurationProvider Build(IConfigurationBuilder builder) 
+            => new DockerSecretsProvider(_secretsPath);
+    }
+}
