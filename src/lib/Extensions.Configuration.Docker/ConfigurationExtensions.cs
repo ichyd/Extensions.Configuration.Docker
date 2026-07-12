8de17a651a3dacffb1f508848bdcf913e8e1f8c7
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-07-11 21:43:18 -0400

    feat: add Docker Secrets configuration provider

---
+++ src/lib/Extensions.Configuration.Docker/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Hisaac.Extensions.Configuration.Docker
+{
+    public static class ConfigurationManagerExtensions
+        {
+            public static IConfigurationBuilder AddDockerSecrets(
+                this IConfigurationBuilder builder, string? secretsPath = null)
+            {
+                ArgumentException.ThrowIfNullOrEmpty(secretsPath);
+                ArgumentException.ThrowIfNullOrWhiteSpace(secretsPath);
+
+                if(!Directory.Exists(secretsPath))
+                    throw new InvalidOperationException(
+                        $"Could not find part of the secrets path: {secretsPath}");
+
+                builder.Add(new DockerSecretsSource(secretsPath));
+
+                return builder;
+            }
+        }
+}
---
+++ src/lib/Extensions.Configuration.Docker/DockerSecretsSource.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Hisaac.Extensions.Configuration.Docker
+{
+    /// <summary>
+    /// Represents a <b>docker secrets</b> source of configuration key/values for an application.
+    /// </summary>
+    public sealed class DockerSecretsSource : IConfigurationSource
+    {
+        private string _secretsPath;
+
+        /// <summary>
+        /// Constructs a new instance of <see cref="DockerSecretsSource"/>.
+        /// </summary>
+        /// <param name="secretsPath">Path to the secrets location. Default is <em>/run/secrets</em>.</param>
+        public DockerSecretsSource(string secretsPath = "/run/secrets")
+        {
+            _secretsPath = secretsPath;
+        }
+
+        /// <inheritdoc/>
+        public IConfigurationProvider Build(IConfigurationBuilder builder) 
+            => new DockerSecretsProvider(_secretsPath);
+    }
+}
